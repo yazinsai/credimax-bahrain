b779f5ba7b2b704406dd5a31cb5f403e8e62cab7
Floating Rock <yazin2k@yahoo.com>
2013-08-29 18:50:29 +0300

    Added Main Page

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Main Page</title>
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-lg-12'>
+				<h1>Set the amount</h1>
+				<form action='./' method='post'>
+					Amount to charge: <input type='text' name='amount'>
+					<button type='submit' class='btn btn-primary'>Pay now</button>
+				</form>
+			</div>
+		</div><!-- ./row -->
+	</div><!-- ./container -->
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</html>
